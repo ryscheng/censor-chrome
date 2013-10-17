7f34e9c998909f4fbb3cce3e90256078697c82c2
Raymond Cheng <RaymondYSCheng@gmail.com>
2013-10-16 22:48:18 -0700

    Updating to allow censor-chrome to reactivate after uproxy turned off

--- js/background.js
+++ js/background.js
@@ -4,11 +4,13 @@ var RULES = {
   'balatarin.com': 'http://123.123.123.123',
   'http://www.nytimes.com/2013/10/14/opinion/the-bay-of-bengal-in-peril-from-climate-change.html': 'http://www.nytimes.com/2013/10/14/opinion/the-bay-of-bengal-in-peril-from-climate-change.htm',
   'http://www.nytimes.com/2013/10/14/us/politics/budget-and-debt-limit-debate.html': 'http://www.nytimes.com/2013/10/14/us/politics/budget-and-debt-limit-debate.htm',
+  'http://www.nytimes.com/2013/10/17/us/congress-budget-debate.html': 'http://www.nytimes.com/2013/10/17/us/congress-budget-debate.htm',
 };
-var state = 0;
+var intendedState = 0;
+var actualState = 0;
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) { 
-  if (state == 1) {
+  if (actualState == 1) {
     var rulesObj = RULES;
     for (var k in rulesObj) {
       if (rulesObj.hasOwnProperty(k) && details.url.indexOf(k) != -1) {
@@ -21,11 +23,13 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
 },{urls: ["<all_urls>"]},["blocking","requestBody"]);
 
 chrome.browserAction.onClicked.addListener(function() {
-  if (state == 0) {
-    state = 1;
+  if (intendedState == 0) {
+    console.log("Button clicked. State transition from off => on");
+    intendedState = 1;
     chrome.browserAction.setIcon({path: 'img/bad.png'});
   } else {
-    state = 0;
+    console.log("Button clicked. State transition from on => off");
+    intendedState = 0;
     chrome.browserAction.setIcon({path: 'img/good.png'});
   }
 });
@@ -33,11 +37,23 @@ chrome.browserAction.onClicked.addListener(function() {
 //Check proxy settings every second and turn blocking off if set
 function checkProxy() {
   console.log('Checking proxy settings');
+  var uproxyOn;
   chrome.proxy.settings.get({'incognito': false}, function(config) {
     if (config && config.value && config.value.mode && 
-         (config.value.mode == 'pac_script' || config.value.mode == 'fixed_servers')) {
+        (config.value.mode == 'pac_script' || config.value.mode == 'fixed_servers')) {
+      uproxyOn = true;
+    } else {
+      uproxyOn = false;
+    }
+    if (uproxyOn && actualState == 1) {
       console.log("Chrome Proxy Settings are set. Turning censorship off");
-      state = 0;
+      actualState = 0;
+    } else if (!uproxyOn && intendedState == 1 && actualState == 0) {
+      console.log("Chrome Proxy Settings are not set. Devil's on");
+      actualState = 1;
+    } else if (!uproxyOn && intendedState == 0 && actualState == 1) {
+      console.log("Chrome Proxy Settings are not set. Alien's on");
+      actualState = 0;
     }
   });
   setTimeout(checkProxy, 1000);
